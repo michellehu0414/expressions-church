b168b7b164fb5a70dbcb82e3866179b5f1922a4d
mchll.h <michellehu0414@users.noreply.github.com>
2025-01-30 16:10:58 -0800

    fix: Update ValuesSection/ folder name to fix previous build fail job

---
+++ src/scss/components/ValuesSection/ValuesSection.js
@@ -0,0 +1,28 @@
+// import './ValuesSection.scss';
+// import valuesData from './values-data.json';
+//
+// export default function ValuesSection() {
+//     // Create container
+//     const scrollContainer = document.createElement('div');
+//     scrollContainer.className = 'scroll-container';
+//
+//     // Create the item container
+//     const itemContainer = document.createElement('ul');
+//     itemContainer.className = 'item-container';
+//
+//     // Populate items from data
+//     valuesData.forEach(({ title, description }) => {
+//         const card = document.createElement('li');
+//         card.className = 'value card';
+//         card.innerHTML = `
+//       <h3>${title}</h3>
+//       <p>${description}</p>
+//     `;
+//         itemContainer.appendChild(card);
+//     });
+//
+//     // Append item container to scroll container
+//     scrollContainer.appendChild(itemContainer);
+//
+//     return scrollContainer;
+// }
