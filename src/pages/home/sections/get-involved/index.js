2eb531a3f89fe5744b6a2349a5e74713df42704b
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-14 14:50:32 -0800

    chore(home): update get-involved.js with full content

--- src/pages/home/sections/get-involved/index.js
+++ src/pages/home/sections/get-involved/index.js
@@ -2,11 +2,15 @@ import { insertGetInvolvedCards } from '@components/GetInvolvedCards';
 
 document.addEventListener('DOMContentLoaded', insertGetInvolvedCards);
 
-document.getElementById("content").innerHTML = `
-<section id="new-section">
-    <h2>Dynamic Section</h2>
-    <p>This section was added using JavaScript!</p>
-</section>
+document.getElementById("get-involved").innerHTML = `
+<div>
+    <div id="partner-card"></div>
+    <div id="fire-relief-card"></div>
+    <div id="give-card"></div>
+    <div id="find-community-card"></div>
+    <div id="serve-card"></div>
+    <div id="serve-sgv-card"></div>
+    </div>
 `;
 
 // Usage - Importing the file
