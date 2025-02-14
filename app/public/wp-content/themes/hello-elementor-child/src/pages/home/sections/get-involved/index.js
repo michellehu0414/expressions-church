f1f19c622eeac29ac972b651cd4c5b69b37fb47c
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-13 17:08:31 -0800

    wip(get-involved): start using innerHTML

--- app/public/wp-content/themes/hello-elementor-child/src/pages/home/sections/get-involved/index.js
+++ app/public/wp-content/themes/hello-elementor-child/src/pages/home/sections/get-involved/index.js
@@ -1,2 +1,13 @@
-import './get-involved.scss';
-console.log("Get involved section loaded");
+import { insertGetInvolvedCards } from '@components/GetInvolvedCards';
+
+document.addEventListener('DOMContentLoaded', insertGetInvolvedCards);
+
+document.getElementById("content").innerHTML = `
+<section id="new-section">
+    <h2>Dynamic Section</h2>
+    <p>This section was added using JavaScript!</p>
+</section>
+`;
+
+// Usage - Importing the file
+// import './src/templates/innerHTML/innerHTML.js';
