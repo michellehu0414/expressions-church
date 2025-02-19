3a806cc6e17ca39e404319d90a61e136173b1fef
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-18 18:10:07 -0800

    refactor(home): import globalComponents script

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,10 @@
 import './home.scss';
+
 import '@js/main';
+import '@components/globalComponents';
+
 import './sections/intro';
 import './sections/get-involved';
+
+// Usage - Importing the file
+// import './src/templates/innerHTML/innerHTML.js';\
