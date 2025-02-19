49cecf1485af687055cb8f16f2b8d9a6eebfb636
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-18 18:08:22 -0800

    refactor(js): remove unused DOMContentLoaded event listener and clean up get-involved section now moved to globalComponents.js

--- src/pages/home/sections/get-involved/index.js
+++ src/pages/home/sections/get-involved/index.js
@@ -1,17 +1,4 @@
-import { insertGetInvolvedCards } from '@components/GetInvolvedCards';
-
-document.addEventListener('DOMContentLoaded', insertGetInvolvedCards);
-
-document.getElementById("get-involved").innerHTML = `
-<div>
-    <div id="partner-card"></div>
-    <div id="fire-relief-card"></div>
-    <div id="give-card"></div>
-    <div id="find-community-card"></div>
-    <div id="serve-card"></div>
-    <div id="serve-sgv-card"></div>
-    </div>
-`;
+import "./get-involved.scss";
 
 // Usage - Importing the file
-// import './src/templates/innerHTML/innerHTML.js';
+// import './src/templates/innerHTML/innerHTML.js';\
