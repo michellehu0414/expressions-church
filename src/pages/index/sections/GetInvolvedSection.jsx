2263cc31d733025f89e5e5151d2fa8e13e427664
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-26 02:06:11 -0800

    feat(GetInvolvedSection): restructure layout and add interest form submission section

--- src/pages/index/sections/GetInvolvedSection.jsx
+++ src/pages/index/sections/GetInvolvedSection.jsx
@@ -3,19 +3,24 @@ import "./GetInvolvedSection.scss";
 import GetInvolvedCards from "@components/GetInvolvedCards/GetInvolvedCards";
 import TeamsAccordionList from "@components/TeamsAccordionList/TeamsAccordionList";
 import ChalkDivider from "@components/ChalkDivider/ChalkDivider";
-import transformStyles from "@scss/utilities/_transform.scss";
+import "@scss/utilities/_all-utilities.scss";
 
 const GetInvolvedSection = () => {
   return (
-    <section className="get-involved-section">
+    <section className="getInvolvedSection">
       <h2><span className="outlined">Get</span> Involved</h2>
       <div className="sectionContent">
         <GetInvolvedCards id="serve-sgv-card" />
         <ChalkDivider variant="white" className="scaleXNeg1" />
         <GetInvolvedCards id="find-community-card" />
         <ChalkDivider variant="white" />
-        <GetInvolvedCards id="serve-card" />
-        <TeamsAccordionList />
+        <div className="serve">
+          <GetInvolvedCards id="serve-card" />
+          <TeamsAccordionList />
+          <h3>Not sure? Let's chat!</h3>
+          <p>Submit an dinterest form anyway, and we'll discern how you can contribute your God-given gifts or talents.</p>
+          <button className="primaryButton bg-white" variant="white" href="/">Submit Interest Form</button>
+        </div>
         <ChalkDivider variant="white" className="rotate180" />
         <GetInvolvedCards id="give-card" />
         <ChalkDivider variant="white" />
@@ -26,7 +31,7 @@ const GetInvolvedSection = () => {
         <GetInvolvedCards id="newcomer-connect-card" />
         <ChalkDivider variant="white" />
       </div>
-    </section>
+    </section >
   );
 };
 
