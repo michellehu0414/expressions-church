b2a38e45bc594c22b778733d83d027c15410de3c
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-26 02:05:35 -0800

    feat(TeamsAccordionList): replace AccordionList with Accordion component and adjust margin styling

--- src/components/TeamsAccordionList/TeamsAccordionList.jsx
+++ src/components/TeamsAccordionList/TeamsAccordionList.jsx
@@ -1,7 +1,8 @@
 // src/pages/About.jsx
 import React, { Component } from "react";
 import PaperBackgroundContainer from "@components/PaperBackgroundContainer/PaperBackgroundContainer";
-import AccordionList from "@components/AccordionList/AccordionList";
+// import AccordionList from "@components/AccordionList/AccordionList";
+import Accordion from "../Accordion/Accordion";
 import "./TeamsAccordionList.scss";
 import { serveTeamsData } from "@data/serveTeamsData"; // ✅ Import data
 
@@ -10,7 +11,11 @@ const TeamsAccordionSection = () => {
     <PaperBackgroundContainer className="teamsContainer"
       headline="Our Teams"  // ✅ Displays a title      // ✅ Shows a divider
       content={
-        <AccordionList items={serveTeamsData} />
+        <section>
+          {serveTeamsData.map((item, index) => (
+            <Accordion key={index} title={item.title} content={item.content} />
+          ))}
+        </section>
       } // ✅ Renders any JSX inside it
     />
   );
