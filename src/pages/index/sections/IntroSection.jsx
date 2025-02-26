bc3643894d52e189b78524caba5c18f12ccf72ef
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-26 02:06:35 -0800

    feat(IntroSection): complete section and update styling for improved readability and structure

--- src/pages/index/sections/IntroSection.jsx
+++ src/pages/index/sections/IntroSection.jsx
@@ -1,41 +1,42 @@
 import React from "react";
 import * as styles from './IntroSection.module.scss';
-import "../../../scss/utilities/_section-components.scss";
+import "@scss/utilities/_section-components.scss";
+import "@scss/utilities/_backgrounds.scss";
+import "@scss/utilities/_spacing.scss";
+import ChalkDivider from "@components/ChalkDivider/ChalkDivider";
+import { sectionWrapper } from "@scss/utilities/_section-components.scss";
 
 const IntroSection = () => {
     return (
         <section className={styles.introSection}>
             <div className="sectionContent">
-                <p className={styles.largeText}>
+                <p id={styles.weBelieve} className={styles.largeText}>
                     We believe Jesus can transform us from the inside out—
                 </p>
                 <div className={styles.christopherHugWrapper}>
                     <img className={styles.imgChristopherHug}
                         src="https://vrwpaviv.elementor.cloud/wp-content/uploads/2024/12/expressions-cutout-hug.png"
                         alt="" />
-                    <div className={styles.cardBGPaper}>
-                        <p>People can’t see God, but they can see his church, and we show that we know
+                    <div id={styles.cardBGPaper} className="bg-paper pd-16">
+                        <p class="black mb-0">People can’t see God, but they can see his church, and we show that we know
                             who
                             God is by how we love others.</p>
                     </div>
                 </div>
                 <p className={styles.largeText}>
                     Our vision ?
-                    <br />
-                    To be an expression of God’s love in the SGV.
                 </p>
                 <img className={styles.imgLaundryLove}
                     src="https://vrwpaviv.elementor.cloud/wp-content/uploads/2025/01/laundry-love.webp"
                     alt="" />
-                <div className={styles.cardBGPaper}>
-                    <p className={styles.largeText}>At Expressions, everyone is invited to explore faith in
-                        Jesus, experience
-                        life with him,
-                        and express love like him.
-                    </p>
+                <p class={styles.largeText}>To express God’s love in the SGV.</p>
+                <div className={styles.taglineWrapper}>
+                    <p className={styles.largeText}><span>Experience</span> life with him.</p>
+                    <p className={styles.largeText}><span>Explore</span> faith in Jesus.</p>
+                    <p className={styles.largeText}> <span>Express</span> love like him.</p>
                 </div>
             </div>
-        </section>
+        </section >
     );
 }
 
