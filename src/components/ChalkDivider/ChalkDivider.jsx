587bcb064cff59d335d7109fee669ade3c99a93c
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-25 21:51:33 -0800

    feat(ChalkDivider): add height prop to customize divider height

--- src/components/ChalkDivider/ChalkDivider.jsx
+++ src/components/ChalkDivider/ChalkDivider.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './ChalkDivider.scss'; // Corrected import statement
 
-const ChalkDivider = ({ variant, className }) => {
+const ChalkDivider = ({ variant, className, height }) => {
     const variantClass = variant ? `${variant}ChalkDivider` : '';
+    const heightStyle = height ? { height: `${height}rem` } : {};
     return (
-        <hr className={`chalkDivider ${className} ${variantClass}`}></hr>
+        <hr className={`chalkDivider ${className} ${variantClass}`} style={heightStyle}></hr>
     );
 };
 
 export default ChalkDivider;
+
+{/* <ChalkDivider variant="black" className= height={ } /> */ }
