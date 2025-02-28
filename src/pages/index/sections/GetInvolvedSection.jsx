84e8277b39856b4ada30c73c3b2a1e9e1c8cb89e
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-27 23:32:51 -0800

    feat(GetInvolvedSection): refactor to use SCSS modules and update component structure

--- src/pages/index/sections/GetInvolvedSection.jsx
+++ src/pages/index/sections/GetInvolvedSection.jsx
@@ -1,34 +1,33 @@
 import React from "react";
-import "./GetInvolvedSection.scss";
+import * as styles from "./GetInvolvedSection.module.scss";
 import GetInvolvedCards from "@components/GetInvolvedCards/GetInvolvedCards";
 import TeamsAccordionList from "@components/TeamsAccordionList/TeamsAccordionList";
 import ChalkDivider from "@components/ChalkDivider/ChalkDivider";
+import Button from "@components/Button/Button";
 import "@scss/utilities/_all-utilities.scss";
 
 const GetInvolvedSection = () => {
   return (
-    <section className="getInvolvedSection">
+    <section>
       <h2><span className="outlined">Get</span> Involved</h2>
       <div className="sectionContent">
-        <GetInvolvedCards id="serve-sgv-card" />
-        <ChalkDivider variant="white" className="scaleXNeg1" />
-        <GetInvolvedCards id="find-community-card" />
-        <ChalkDivider variant="white" />
-        <div className="serve">
-          <GetInvolvedCards id="serve-card" />
+        <GetInvolvedCards id="serve-sgv-card" className={styles.card} />
+        <GetInvolvedCards id="find-community-card" className={styles.card}  />
+        <div>
+          <GetInvolvedCards id="serve-card" className={styles.card}  />
           <TeamsAccordionList />
           <h3>Not sure? Let's chat!</h3>
           <p>Submit an dinterest form anyway, and we'll discern how you can contribute your God-given gifts or talents.</p>
-          <button className="primaryButton bg-white" variant="white" href="/">Submit Interest Form</button>
+          <Button href="/" variant="primary" className="bgWhite">
+            Submit Interest Form
+          </Button>
         </div>
-        <ChalkDivider variant="white" className="rotate180" />
-        <GetInvolvedCards id="give-card" />
-        <ChalkDivider variant="white" />
-        <GetInvolvedCards id="fire-relief-card" />
-        <ChalkDivider variant="white" className="transformNone" />
-        <GetInvolvedCards id="partner-card" />
-        <ChalkDivider variant="white" />
-        <GetInvolvedCards id="newcomer-connect-card" />
+        <GetInvolvedCards id="give-card" className={styles.card}  />
+
+        <GetInvolvedCards id="fire-relief-card" className={styles.card}  />
+        <GetInvolvedCards id="partner-card" className={styles.card}  />
+
+        <GetInvolvedCards id="newcomer-connect-card" className={styles.card}  />
       </div>
     </section >
   );
