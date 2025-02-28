dff0e7ba07c43855ffc05fb373366f3f3e1a2ca3
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-27 23:29:57 -0800

    feat(EventsSection): refactor to use SCSS modules for styling and update component structure

--- src/pages/index/sections/EventsSection.jsx
+++ src/pages/index/sections/EventsSection.jsx
@@ -1,2 +1,10 @@
-import SubsplashEventsEmbed from "@components/SubsplashEventsEmbed";
-import "./EventsSection.scss";
+import React from 'react';
+import * as styles from './EventsSection.module.scss';
+import SubsplashEventsEmbed from "@components/SubsplashEventsEmbed/SubsplashEventsEmbed";
+
+const EventsSection = () => {
+    return (
+        <SubsplashEventsEmbed />
+    );
+};
+export default EventsSection;
