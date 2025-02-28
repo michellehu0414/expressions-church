57ccf4b7b845d04d6462857b5e3f15eca0f39a3a
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-27 23:33:20 -0800

    feat(PYVSection): add new section component with styles for visit planning

---
+++ src/pages/index/sections/PYVSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './PYVSection.scss';
+import "@scss/utilities/_all-utilities.scss";
+import churchMembersCutout from "@images/church-members-mixed-cutouts.png";
+import Button from "@components/Button/Button";
+
+const PYVSection = () => {
+    return (
+        <section>
+            <h2>
+                <span className="outlined">Ready to</span>
+                <br />
+                plan a visit?
+            </h2>
+            <div className="sectionContent">
+                <img src={churchMembersCutout} alt="" className="churchMembersCutout" />
+            </div>
+
+        </section>
+    );
+};
+export default PYVSection;
