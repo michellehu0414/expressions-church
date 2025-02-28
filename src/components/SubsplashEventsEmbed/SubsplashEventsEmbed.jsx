f3716a91bf0fb5e20279f56a8f822603c0e00023
mchll.h <michellehu0414@users.noreply.github.com>
2025-02-27 23:29:34 -0800

    feat(SubsplashEventsEmbed): create new component with SCSS module for styling

---
+++ src/components/SubsplashEventsEmbed/SubsplashEventsEmbed.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import * as styles from './SubsplashEventsEmbed.module.scss';
+
+const SubsplashEventsEmbed = () => {
+    return (
+        <div className={`${styles.subsplashEmbedEvents} container-max-width`} >
+        </div >
+    );
+};
+export default SubsplashEventsEmbed;
