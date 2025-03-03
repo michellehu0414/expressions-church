6536e8907159b8aafa49562e3f613a892aa48787
mchll.h <michellehu0414@users.noreply.github.com>
2025-03-03 11:59:11 -0800

    refactor(EventsSection): remove unused styles import; update button margin for better alignment

--- src/pages/index/sections/EventsSection.jsx
+++ src/pages/index/sections/EventsSection.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as styles from './EventsSection.module.scss';
 import SubsplashEventsEmbed from "@components/SubsplashEventsEmbed/SubsplashEventsEmbed";
 
 const EventsSection = () => {
