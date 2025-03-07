101859534f4e57512f072e28a1a9df564501223e
michellehu0414 <103789293+michellehu0414@users.noreply.github.com>
2025-03-07 00:43:53 -0800

    feat(index): change Supsplash event embedding from dynamic script loading to static script loading;

--- src/components/SubsplashEventsEmbed/SubsplashEventsEmbed.jsx
+++ src/components/SubsplashEventsEmbed/SubsplashEventsEmbed.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react';
-import * as styles from './SubsplashEventsEmbed.module.scss';
 
 const SubsplashEventsEmbed = () => {
-    return (
-        <div className={`${styles.subsplashEmbedEvents} container-max-width`} >
-        </div >
-    );
+    useEffect(() => {
+        const target = document.getElementById('subsplash-embed-qtbc9rz');
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = function () {
+          subsplashEmbed('+dwm2/lb/ca/+qtbc9rz?embed', 'https://subsplash.com/', 'subsplash-embed-qtbc9rz');
+        };
+        script.src = 'https://dashboard.static.subsplash.com/production/web-client/external/embed-1.1.0.js';
+        target.parentElement.insertBefore(script, target);
+      }, []);
+
+      return <div id="subsplash-embed-qtbc9rz"></div>;
 };
+
 export default SubsplashEventsEmbed;
