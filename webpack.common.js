b803ea15d2d918859662da26c7060afd279ae8fc
mchll.h <michellehu0414@users.noreply.github.com>
2025-03-19 21:09:07 -0700

    chore: Refactor webpack configuration for new vanilla JavaScript structure; use webpack-merge and streamline development setup

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,27 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const htmlPages = [
-    { template: '/index.html', chunks: ['index'], filename: 'index.html' },
-    { template: '/leadership.html', chunks: ['leadership'], filename: 'leadership.html' },
-    { template: '/plan-your-visit.html', chunks: ['plan-your-visit'], filename: 'plan-your-visit.html' },
-    { template: '/events.html', chunks: ['events'], filename: 'events.html' },
+const pages = [
+    { name: "index", title: "Home" },
+    { name: "small-groups", title: "Small Groups" },
+    { name: "leadership", title: "Leadership" },
+    // { name: "plan-your-visit", title: "Plan Your Visit" },
+    // { name: "events", title: "Events" },
+    // { name: "fire-relief", title: "Fire Relief" }
 ];
 
 module.exports = {
     entry: {
-        index: "./src/pages/index/index.jsx",
-        leadership: "./src/pages/leadership/index.jsx",
-        events: "./src/pages/events/index.jsx",
-        pyv: "./src/pages/plan-your-visit/index.jsx",
-        main: "./src/main.jsx",
+        index: "./src/pages/index/index.js",
+        leadership: "./src/pages/leadership/leadership.js",
+        smallGroups: "./src/pages/small-groups/small-groups.js",
+        // fireRelief: "./src/pages/fire-relief/index.jsx",
+        // events: "./src/pages/events/index.jsx",
+        // pyv: "./src/pages/plan-your-visit/index.jsx",
+        main: "./src/main.js",
     },
     output: {
         filename: "js/[name].min.js",
@@ -27,60 +32,47 @@ module.exports = {
         alias: {
             "@src": path.resolve(__dirname, "src"),
             "@assets": path.resolve(__dirname, "src/assets/"),
-            "@svg": path.resolve(__dirname, "src/assets/svg/"),
+            "@data": path.resolve(__dirname, "src/data/"),
             "@scss": path.resolve(__dirname, "src/scss/"),
-            "@abstracts": path.resolve(__dirname, "src/scss/abstracts/"),
-            "@base": path.resolve(__dirname, "src/scss/base/"),
-            "@utilities": path.resolve(__dirname, "src/scss/utilities/"),
-            "@widgets": path.resolve(__dirname, "src/scss/elementorWidgets/"),
-            "@js": path.resolve(__dirname, "src/js/"),
             "@components": path.resolve(__dirname, "src/components/"),
             "@pages": path.resolve(__dirname, "src/pages/"),
-            "@utils": path.resolve(__dirname, "src/utils/"),
-            "@data": path.resolve(__dirname, "src/data/"),
         },
-        extensions: [".js", ".jsx", ".json", ".scss"],
+        extensions: [".js", ".json", ".scss", ".css", ".jsx", ".ts"],
     },
+
     module: {
         rules: [
             {
-                test: /\.(woff|woff2)$/,
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
-                generator: {
-                    filename: "assets/fonts/[name][ext]",
-                },
+                generator: { filename: "assets/fonts/[name][ext]" },
             },
             {
-                test: /\.module\.scss$/,
+                test: /\.html$/,
                 use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                localIdentName:
-                                    process.env.NODE_ENV === "production"
-                                        ? "[hash:base64:5]"
-                                        : "[path][name]__[local]",
-                            },
-                        },
-                    },
-                    "postcss-loader",
+                    "raw-loader",
+                ],
+            },
+            {
+                test: /\.styles.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    "sass-to-string",
                     {
                         loader: "sass-loader",
                         options: {
-                            implementation: require("sass"), // Use Dart Sass
                             sassOptions: {
                                 includePaths: [path.resolve(__dirname, "src/scss")],
                                 outputStyle: "expanded",
                             },
                         },
                     },
-                ],
+                ]
             },
             {
-                test: /\.scss$/,
-                exclude: /\.module\.scss$/,
+                test: /\.(scss|css)$/,
+                // Excluding the `.styles.scss` extension
+                exclude: [/\.styles.scss$/, /node_modules/],
                 use: [
                     MiniCssExtractPlugin.loader,
                     "css-loader",
@@ -88,7 +80,7 @@ module.exports = {
                     {
                         loader: "sass-loader",
                         options: {
-                            implementation: require("sass"), // Use Dart Sass
+                            implementation: require("sass"),
                             sassOptions: {
                                 includePaths: [path.resolve(__dirname, "src/scss")],
                                 outputStyle: "expanded",
@@ -98,52 +90,30 @@ module.exports = {
                 ],
             },
             {
-                test: /\.jsx?$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: [
-                    'thread-loader',
+                    "thread-loader",
                     {
-                        loader: 'babel-loader',
+                        loader: "babel-loader",
                         options: {
                             cacheDirectory: true,
-                        },
+                            presets: ['@babel/preset-env']
+                        }
                     },
                 ],
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
                 generator: {
-                    filename: 'assets/images/[name][ext]'
+                    filename: "assets/images/[name][ext]"
                 },
-                use: [
-                    {
-                        loader: 'image-webpack-loader', // Optimize images
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    }
-                ]
             },
             {
                 test: /\.svg$/,
                 use: [
+                    "raw-loader",
                     {
                         loader: 'svgo-loader',
                         options: {
@@ -158,27 +128,21 @@ module.exports = {
             },
         ],
     },
+
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "css/[name].min.css",
-        }),
-        ...htmlPages.map(page => new HtmlWebpackPlugin({
-            template: `./src/${page.template}`,
-            filename: page.filename,
-            chunks: page.chunks,
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "css/[name].min.css" }),
+        ...pages.map(page => new HtmlWebpackPlugin({
+            template: `./src/pages/${page.name}/${page.name}.html`,
+            filename: `${page.name}.html`,
+            chunks: [page.name, "main"],
+            title: page.title,
         })),
         new CopyWebpackPlugin({
             patterns: [
-                {
-                    from: 'src/assets/images',
-                    to: 'assets/images'
-                },
-                {
-                    from: 'src/assets/svg',
-                    to: 'assets/svg'
-                },
-            ]
+                { from: "src/assets/images", to: "assets/images" },
+                { from: "src/assets/svg", to: "assets/svg" },
+            ],
         }),
-
     ],
 };
